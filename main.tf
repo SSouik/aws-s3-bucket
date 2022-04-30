3201b07f809c9e6306c0a62a08164631c1ee6427
Sam Souik <me@samuelsouik.com>
2022-04-29 23:12:15 -0500

    added: bucket logging and setup (#7)

--- main.tf
+++ main.tf
@@ -44,3 +44,23 @@ resource "aws_s3_bucket_website_configuration" "main" {
     key = var.website_config.error_document
   }
 }
+
+# Bucket Logging
+resource "aws_s3_bucket" "main_log_bucket" {
+  count  = var.log_bucket_name != null ? 1 : 0
+  bucket = var.log_bucket_name
+}
+
+resource "aws_s3_bucket_acl" "main_log_bucket" {
+  count  = var.log_bucket_name != null ? 1 : 0
+  bucket = aws_s3_bucket.main_log_bucket[count.index].id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "main" {
+  count  = var.log_bucket_name != null ? 1 : 0
+  bucket = aws_s3_bucket.main.id
+
+  target_bucket = aws_s3_bucket.main_log_bucket[count.index].id
+  target_prefix = var.log_prefix
+}
